217e9de9df74e1ae71981c16457418c524036d8d
Nico Pfiffner (nipfif) <n.pfiffner@ikmz.uzh.ch>
2021-12-08 23:20:31 +0100

    Temporarily disabled dotenv.

--- ddm/settings.py
+++ ddm/settings.py
@@ -1,9 +1,10 @@
 import os
 import pytz
 from dotenv import load_dotenv
+from django.conf import settings
 
-load_dotenv()
+#load_dotenv()
 
 SQ_TIMEZONE = pytz.timezone("Europe/Zurich")
 
-ENCRYPTION_KEY = os.environ['ENCRYPTION_KEY']
+ENCRYPTION_KEY = settings.SECRET_KEY #os.environ['ENCRYPTION_KEY']

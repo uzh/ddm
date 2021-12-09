031dd27f4335e461ce4034fe887dba5171f63678
Nico Pfiffner (nipfif) <n.pfiffner@ikmz.uzh.ch>
2021-12-09 22:33:54 +0100

    Split views.py into two files to improve code structure and overview.

---
+++ ddm/views/__init__.py
@@ -0,0 +1,8 @@
+from .data_processing import (
+    process_view, export_file, delete_questionnaire_responses
+)
+
+from .questionnaire import (
+    QuestionnaireOverview, QuestionnaireThankYou, QuestionnaireAlreadyCompleted,
+    questionnaire_continue, questionnaire_admission, display_questionnaire
+)

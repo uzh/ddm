4539dd86ddd7929870a627f133e468295e51c2bc
Nico Pfiffner (nipfif) <n.pfiffner@ikmz.uzh.ch>
2021-12-20 16:15:55 +0100

    Defined default primary key type as 'django.db.models.AutoField'.

--- ddm/apps.py
+++ ddm/apps.py
@@ -2,6 +2,7 @@ from django.apps import AppConfig
 
 
 class DdmConfig(AppConfig):
+    default_auto_field = 'django.db.models.AutoField'
     name = 'ddm'
     verbose_name = 'Data Donation Module'
 

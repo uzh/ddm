aac4e18cbef8a04c81f463442309c77a1b57bd2d
Nico Pfiffner (nipfif) <n.pfiffner@ikmz.uzh.ch>
2022-02-18 15:27:26 +0100

    Deleted deprecated files.

--- ddm/admin.py
+++ ddm/admin.py
@@ -1,16 +1,17 @@
-from django.contrib import admin
-from django.shortcuts import redirect
-from django.urls import reverse
-from ddm.models import Questionnaire
-
-
-class QuestionnaireAdmin(admin.ModelAdmin):
-    """
-    Class to hook surquest into admin menu. Redirects to view defined in
-    admin_views
-    """
-    def changelist_view(self, request, extra_context=None):
-        return redirect(reverse('questionnaire-list'))
-
-
-admin.site.register(Questionnaire, QuestionnaireAdmin)
+# TODO: Check if any of this is still needed.
+# from django.contrib import admin
+# from django.shortcuts import redirect
+# from django.urls import reverse
+# from ddm.models import Questionnaire
+#
+#
+# class QuestionnaireAdmin(admin.ModelAdmin):
+#     """
+#     Class to hook surquest into admin menu. Redirects to view defined in
+#     admin_views
+#     """
+#     def changelist_view(self, request, extra_context=None):
+#         return redirect(reverse('questionnaire-list'))
+#
+#
+# admin.site.register(Questionnaire, QuestionnaireAdmin)
--- ddm/settings.py
+++ ddm/settings.py
@@ -1,24 +1,4 @@
-import os
 import pytz
-from dotenv import load_dotenv
-from django.conf import settings
 
-#load_dotenv()
-
+# TODO: Check if this belongs here or if it should be move.
 SQ_TIMEZONE = pytz.timezone("Europe/Zurich")
-
-ENCRYPTION_KEY = settings.SECRET_KEY #os.environ['ENCRYPTION_KEY']
-
-# VUE_FRONTEND_DIR = os.path.join(os.path.dirname(os.path.dirname(os.getcwd())), 'vue_frontend')
-# print(VUE_FRONTEND_DIR)
-#
-# WEBPACK_LOADER = {
-#     'DEFAULT': {
-#         'CACHE': not settings.DEBUG,
-#         'BUNDLE_DIR_NAME': 'vue/',  # must end with slash
-#         'STATS_FILE': os.path.join(VUE_FRONTEND_DIR, 'webpack-stats.json'),
-#         'POLL_INTERVAL': 0.1,
-#         'TIMEOUT': None,
-#         'IGNORE': [r'.+\.hot-update.js', r'.+\.map']
-#     }
-# }

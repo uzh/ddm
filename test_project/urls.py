dad128db309a451595daa76f5bb65f92253fd52c
Nico Pfiffner (nipfif) <n.pfiffner@ikmz.uzh.ch>
2022-02-18 16:49:42 +0100

    Relocated urls from test project ot module.

--- test_project/urls.py
+++ test_project/urls.py
@@ -4,17 +4,9 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
 
-from ddm.views import (
-    DataUpload, ProjectEntry, QuestionnaireDisplay, ProjectExit
-)
-
 urlpatterns = [
     path(r'admin/', admin.site.urls),
     path('', include('ddm.urls')),
-    path(r'<slug:slug>/intro/', ProjectEntry.as_view(), name='project-entry'), # TODO: Move this to the DDM module.
-    path(r'<slug:slug>/data-donation/', DataUpload.as_view(), name='data-donation'),  # TODO: Move this to the DDM module.
-    path(r'<slug:slug>/questionnaire/', QuestionnaireDisplay.as_view(), name='questionnaire'),  # TODO: Move this to the DDM module.
-    path(r'<slug:slug>/end/', ProjectExit.as_view(), name='project-exit'),  # TODO: Move this to the DDM module.
 ]
 
 if settings.DEBUG:

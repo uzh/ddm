8be724888c3d9e32910665fe1912d37db76247de
Nico Pfiffner (nipfif) <n.pfiffner@ikmz.uzh.ch>
2022-03-17 14:28:10 +0100

    test: Added structure for encrypted field.

---
+++ ddm/tests/encrypted_field.py
@@ -0,0 +1,13 @@
+from django.test import TestCase
+from ddm.models import EncryptedField
+
+
+class TestEncryptedFieldIsolated(TestCase):
+    def test_get_prep_value(self):
+        pass
+
+    def test_from_db_value(self):
+        pass
+
+    def test_to_python(self):
+        pass

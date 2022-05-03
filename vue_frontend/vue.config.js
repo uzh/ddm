24e1369f539094f54274da28808473fab9b22393
Nico Pfiffner (nipfif) <n.pfiffner@ikmz.uzh.ch>
2022-05-03 08:26:23 +0200

    vue: Relocate production files.

--- vue_frontend/vue.config.js
+++ vue_frontend/vue.config.js
@@ -19,7 +19,7 @@ module.exports = {
     publicPath: process.env.NODE_ENV === 'production'
         ? ''
         : 'http://localhost:8080/',
-    outputDir: path.resolve('../ddm/static/ddm/'),
+    outputDir: path.resolve('../ddm/static/ddm/vue'),
 
     chainWebpack: config => {
 

f76522b73be30ea7ed46bfbcc8620028e8f05500
Pim Witlox <pim.witlox@uzh.ch>
2022-06-20 15:41:57 +0200

    hybrid approach for larger data

--- ddm/models/encryption.py
+++ ddm/models/encryption.py
@@ -1,6 +1,7 @@
 from Crypto.Cipher import PKCS1_OAEP
 from Crypto.Hash import HMAC
 from Crypto.PublicKey import RSA
+from io import BytesIO
 from struct import pack
 
 import binascii
@@ -37,17 +38,26 @@ class Encryption:
         Save encrypted data to the database, either use a supplied 'public_key'
         or generate the material dynamically.
         """
-        rsa_material = RSA.importKey(self.public_key)
-        cipher = PKCS1_OAEP.new(rsa_material)
-        return base64.b64encode(cipher.encrypt(bytes(json.dumps(value), 'utf-8')))
+        session_key = get_random_bytes(16)
+        cipher_aes = AES.new(session_key, AES.MODE_EAX)
+        cipher_data, tag = cipher_aes.encrypt_and_digest(bytes(json.dumps(value), 'utf-8')
+        cipher_rsa  = PKCS1_OAEP.new(RSA.importKey(self.public_key))
+        encrypted_session_key = cipher_rsa.encrypt(session_key)
+        payload = encrypted_session_key + cipher_aes.nonce + tag + cipher_data
+        return base64.encodebytes(payload)
 
     def decrypt(self, value):
         """
         Fetch encrypted data from the database, try to decode the stored data,
         None if decryption fails.
         """
-        cipher = PKCS1_OAEP.new(self.rsa)
-        try:
-            return json.loads(cipher.decrypt(base64.b64decode(value)).decode('utf-8'))
-        except binascii.Error:
-            return None
+        value_bytes = BytesIO(base64.decodebytes(value))
+        encrypted_session_key = value_bytes.read(self.rsa.size_in_bytes())
+        nonce = value_bytes.read(16)
+        tag = value_bytes.read(16)
+        cipherdata = encrypted_bytes.read()
+        session_key = PKCS1_OAEP.new(self.rsa.exportKey()).decrypt(encrypted_session_key)
+        cipher_aes = AES.new(session_key, AES.MODE_EAX, nonce)
+        decrypted = cipher_aes.decrypt_and_verify(cipherdata, tag)
+        return json.loads(decrypted).decode('utf-8')
+

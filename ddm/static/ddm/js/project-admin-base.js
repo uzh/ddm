f89c250bdbe9a021e782953fc45c2835f3933257
Nico Pfiffner (nipfif) <n.pfiffner@ikmz.uzh.ch>
2022-06-30 18:45:16 +0200

    chore(UI): Various template updates.

--- ddm/static/ddm/js/project-admin-base.js
+++ ddm/static/ddm/js/project-admin-base.js
@@ -2,3 +2,12 @@ function closeMessage(messageID) {
     const element = document.getElementById(messageID);
     element.remove();
 }
+
+$( document ).ready(function() {
+    $("input[type=checkbox]").each(function() {
+        let $lbl = $("<label>");
+        $lbl.attr("for", this.id);
+        $lbl.attr("class", "checkmark")
+        $(this).after($lbl);
+    });
+});

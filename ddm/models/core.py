204497cd7dedf08664009cc2473dcf5f57adc5f9
Nico Pfiffner (nipfif) <n.pfiffner@ikmz.uzh.ch>
2022-10-13 15:39:28 +0200

    fix: Zero-division error in project statistics.

--- ddm/models/core.py
+++ ddm/models/core.py
@@ -127,7 +127,7 @@ class DonationProject(models.Model):
         statistics = {
             'n_started': participants.count(),
             'n_completed': participants.filter(completed=True).count(),
-            'completion_rate': participants.filter(completed=True).count() / participants.count(),
+            'completion_rate': participants.filter(completed=True).count() / participants.count() if participants.count() > 0 else 0,
             'n_donations': DataDonation.objects.filter(project=self).count(),
             'n_errors': ddm_exceptions.ExceptionLogEntry.objects.filter(project=self).count(),
             'average_time': str(participants.filter(completed=True).aggregate(v=Avg(F('end_time')-F('start_time')))['v']).split(".")[0]

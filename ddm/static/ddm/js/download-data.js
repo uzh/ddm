326e0b9e9b547d34e50ed0d09bae6ed2cb9ffb2f
Nico Pfiffner (nipfif) <n.pfiffner@ikmz.uzh.ch>
2022-12-19 14:27:29 +0100

    ui: Show overlay during json download preparation.

--- ddm/static/ddm/js/download-data.js
+++ ddm/static/ddm/js/download-data.js
@@ -1,14 +1,20 @@
 function downloadData(postUrl, fileName) {
-    $.ajax({
+  $.ajax({
         type: 'GET',
         dataType: 'json',
         url: postUrl,
+        beforeSend: function () {
+          $("#download-overlay").toggle();
+        },
         success: function (data, status, result) {
-            let blob = new Blob([JSON.stringify(data)], {type: 'octet/stream'});
-            let link=document.createElement('a');
-            link.href=window.URL.createObjectURL(blob);
-            link.download=fileName.concat('.json');
-            link.click();
+          let blob = new Blob([JSON.stringify(data)], {type: 'octet/stream'});
+          let link=document.createElement('a');
+          link.href=window.URL.createObjectURL(blob);
+          link.download=fileName.concat('.json');
+          link.click();
+        },
+        complete: function () {
+          $("#download-overlay").toggle();
         }
     });
 }

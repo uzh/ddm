c396ad17b6b3931dc35087b6f69e4109a1d03d88
Nico Pfiffner (nipfif) <n.pfiffner@ikmz.uzh.ch>
2023-01-20 17:11:48 +0100

    refactor: Update to handle zip files.

--- ddm/static/ddm/js/download-data.js
+++ ddm/static/ddm/js/download-data.js
@@ -13,19 +13,22 @@ function downloadData(postUrl, fileName, get_secret=false) {
     $('#secret-input').val('');
     $('#modal-secret-input').modal('toggle');
   }
+
   $.ajax({
     type: 'GET',
-    dataType: 'json',
     url: postUrl,
     headers: {'Super-Secret': secret},
+    xhrFields:{
+      responseType: 'blob'
+    },
     beforeSend: function (xhr) {
       $('#download-overlay').toggle();
     },
     success: function (data, status, result) {
-      let blob = new Blob([JSON.stringify(data)], {type: 'octet/stream'});
+      let blob = new Blob([data], {type: 'application/zip'});
       let link = document.createElement('a');
       link.href = window.URL.createObjectURL(blob);
-      link.download = fileName.concat('.json');
+      link.download = fileName.concat('.zip');
       link.click();
       $('#download-overlay').toggle();
     },

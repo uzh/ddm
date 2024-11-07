25984df844a09cae03fe96df388cdbafc27455d6
Nico-AP <n.pfiffner@ikmz.uzh.ch>
2024-11-07 13:26:27 +0100

    refactor: Move main urls.py from ddm root to ddm.core.

--- test_project/urls.py
+++ test_project/urls.py
@@ -8,7 +8,7 @@ from django.urls import path
 
 urlpatterns = [
     path(r'admin/', admin.site.urls),
-    path('', include('ddm.urls')),
+    path('', include('ddm.core.urls')),
     path('login/', auth_views.LoginView.as_view(template_name='auth/login.html'), name='ddm-login'),
     path('logout/', auth_views.LogoutView.as_view(), name='ddm-logout'),
     path('ckeditor5/', include('django_ckeditor_5.urls')),

c7ea3f605427cf8953e98966ce60c006e9d47d1d
Nico-AP <n.pfiffner@ikmz.uzh.ch>
2024-11-15 14:05:50 +0100

    chore: Update gh actions and corresponding setting and docs configuration.

--- test_project/settings.py
+++ test_project/settings.py
@@ -2,6 +2,9 @@ import json
 import os
 import sys
 
+from dotenv import load_dotenv
+
+load_dotenv()
 
 PROJECT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 DDM_DIR = os.path.join(PROJECT_DIR, 'ddm')
@@ -9,8 +12,6 @@ VUE_FRONTEND_DIR = os.path.join(PROJECT_DIR, 'frontend')
 
 sys.path.append(PROJECT_DIR)
 sys.path.append(DDM_DIR)
-
-test_config = json.load(open(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'test_config.json')))
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -69,19 +70,24 @@ TEMPLATES = [
 
 DB_CONFIG = {
     'default': {
-        'ENGINE': test_config['DB_ENGINE'],
-        'NAME': test_config['DB_NAME'],
+        'ENGINE': os.environ.get('DB_ENGINE', 'django.db.backends.sqlite3'),
+        'NAME': os.environ.get('DB_NAME', 'ddmtestdb'),
+        'USER': os.environ.get('DB_USER', 'ddmtestuser'),
+        'PASSWORD': os.environ.get('DB_PASSWORD'),
+        'HOST': os.environ.get('DB_HOST', ''),
     }
 }
+DATABASES = DB_CONFIG
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+SECRET_KEY = os.environ.get('SECRET_KEY')
 ROOT_URLCONF = 'urls'
-DATABASES = DB_CONFIG
 DEBUG = True
-SECRET_KEY = test_config['SECRET_KEY']
 SITE_ID = 1
+
 USE_TZ = True
 TIME_ZONE = 'Europe/Zurich'
+
 LANGUAGE_CODE = 'en'
 USE_I18N = True
 LANGUAGES = [
@@ -108,10 +114,6 @@ WEBPACK_LOADER = {
     }
 }
 
-DDM_SETTINGS = {
-    'EMAIL_PERMISSION_CHECK': r'.*(\.|@)uzh\.ch$',
-}
-
 AUTH_PASSWORD_VALIDATORS = [
     {'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator', },
     {'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator', },
@@ -119,14 +121,17 @@ AUTH_PASSWORD_VALIDATORS = [
     {'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator', },
 ]
 
+DDM_SETTINGS = {
+    'EMAIL_PERMISSION_CHECK': r'.*(\.|@)uzh\.ch$',
+}
+
 LOGIN_REDIRECT_URL = '/projects/'
 LOGOUT_REDIRECT_URL = '/login/'
 
-DDM_DEFAULT_HEADER_IMG_LEFT = '/media/project_10/headers/DDLabLogo.png'
-DDM_DEFAULT_HEADER_IMG_RIGHT = '/media/project_10/headers/IKMZ_Logo.png'
+DDM_DEFAULT_HEADER_IMG_LEFT = ''
+DDM_DEFAULT_HEADER_IMG_RIGHT = ''
 
 # INTERNAL_IPS = ["127.0.0.1", ]  # Added for debugging purposes
-
 
 # ckeditor 5 configuration
 CKEDITOR_5_FILE_UPLOAD_PERMISSION = 'authenticated'

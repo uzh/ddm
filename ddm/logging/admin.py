84c0ccb880b2773e86894037bcfeed4da7cf5343
Nico-AP <n.pfiffner@ikmz.uzh.ch>
2025-06-24 00:14:42 +0200

    chore: Update DDM Exception Logs admin view to add filter options and additional information.

--- ddm/logging/admin.py
+++ ddm/logging/admin.py
@@ -1,4 +1,8 @@
+import json
+
 from django.contrib import admin
+from django.utils.html import format_html
+
 from ddm.logging.models import ExceptionLogEntry, EventLogEntry
 
 
@@ -7,11 +11,26 @@ class ExceptionsAdmin(admin.ModelAdmin):
     Provides an overview of all registered exceptions.
     """
     list_display = [
-        'formatted_date', 'project', 'raised_by', 'exception_type', 'message'
+        'formatted_date', 'project', 'participant', 'raised_by',
+        'exception_type', 'formatted_message'
+    ]
+    list_filter = [
+        'project', 'exception_type', 'raised_by'
     ]
 
     def has_add_permission(self, request, obj=None):
         return False
+
+    @admin.display(description='Message')
+    def formatted_message(self, obj):
+        msg = obj.message
+        try:
+            msg_json = json.loads(msg)
+            msg = json.dumps(msg_json, indent=4, ensure_ascii=False)
+            return format_html('<pre>{}</pre>', msg)
+        except ValueError:
+            msg = msg
+        return msg
 
     @admin.display(ordering='date', description='Date')
     def formatted_date(self, obj):

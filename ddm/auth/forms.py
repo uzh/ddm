aca8cc681627afb190e1f4edcbf8fe258507efc6
Nico-AP <n.pfiffner@ikmz.uzh.ch>
2025-06-24 01:10:11 +0200

    fix: Fix API token form to prevent overwriting of existing tokens and fix error when deleting active tokens.

--- ddm/auth/forms.py
+++ ddm/auth/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from django.core.exceptions import ValidationError
 
 
 class TokenCreationForm(forms.Form):
@@ -6,10 +7,19 @@ class TokenCreationForm(forms.Form):
         initial=30,
         min_value=1,
         max_value=90,
-        required=True
+        required=False,
     )
     action = forms.CharField(
         max_length=20,
         initial='create',
         widget=forms.HiddenInput()
     )
+
+    def clean(self):
+        cleaned_data = super().clean()
+        action = cleaned_data.get('action')
+
+        if action == 'create' and not cleaned_data.get('expiration_days'):
+            raise ValidationError("Expiration days required when creating token.")
+
+        return cleaned_data

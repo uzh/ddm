35d779aa36d47272c76ad42b7e13bd4faaa7208e
Nico-AP <n.pfiffner@ikmz.uzh.ch>
2025-06-24 19:44:49 +0200

    chore: Prepare version bump for new release.

--- version_bump.sh
+++ version_bump.sh
@@ -1,7 +1,7 @@
 # pip install django-ddm
 # prev=$(pip show django-ddm | grep Version: | cut -d' ' -f2 )
-minor=2
-major=2.0
+minor=0
+major=2.1
 # let "minor++"
 sed -i "s/__version__/$major.$minor/g" setup.cfg
 sed -i "s/__version__/$major.$minor/g" ddm/__init__.py

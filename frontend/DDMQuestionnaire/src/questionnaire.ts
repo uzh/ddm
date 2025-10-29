18bd9ddd321a67f7ebb0014b494d0dd2c416b569
Nico-AP <n.pfiffner@ikmz.uzh.ch>
2025-10-29 10:19:12 +0100

    fix: Fix initialization of browser language in Questionnaire component.

--- frontend/DDMQuestionnaire/src/questionnaire.ts
+++ frontend/DDMQuestionnaire/src/questionnaire.ts
@@ -8,10 +8,16 @@ import de from './locales/de.json';
 import it from './locales/it.json';
 import fr from './locales/fr.json';
 
+const APP_CONFIG = {
+  selector: '#qapp',
+  defaultLocale: 'en',
+  fallbackLocale: 'en',
+}
+
 const i18n = createI18n({
   legacy: false,
-  locale: 'en',
-  fallbackLocale: 'en',
+  locale: APP_CONFIG.defaultLocale,
+  fallbackLocale: APP_CONFIG.fallbackLocale,
   messages: {
     en,
     de,
@@ -20,9 +26,19 @@ const i18n = createI18n({
   }
 })
 
-const selector = "#qapp";
+const selector = APP_CONFIG.selector;
 const mountEl = document.querySelector(selector) as HTMLElement;
 const app = createApp(QApp, {...mountEl.dataset})
+
+// Set i18n locale based on the data attribute.
+const userLanguage = mountEl.dataset.language || APP_CONFIG.defaultLocale;
+const availableLocales = i18n.global.availableLocales as string[];
+
+if (availableLocales.includes(userLanguage)) {
+  i18n.global.locale.value = userLanguage as 'en' | 'de' | 'it' | 'fr';
+} else {
+  i18n.global.locale.value = APP_CONFIG.fallbackLocale as 'en' | 'de' | 'it' | 'fr';
+}
 
 app.directive('only-digits', onlyDigits);
 app.directive('valid-email', validEmail);
